10b1645c3336d4a8321633d42d9dbe787cf4e68c
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-07-27 08:02:01 +0300

    +

--- src/js/hero_background.js
+++ src/js/hero_background.js
@@ -3,7 +3,6 @@ import { generatePictureTag } from './generatepicturetag.js';
 import { getLocalData, setLocalData } from './localdata.js';
 import { refs } from './refs.js';
 
-// const input = document.querySelector('.inputid');
 const localId = getLocalData();
 const theme = colorTheme.find(theme => theme.id === localId);
 
@@ -17,16 +16,3 @@ refs.heroBackground.insertAdjacentHTML(
   'beforeend',
   generatePictureTag(heroBackground, theme.path, theme.path_retina, 'background_image')
 );
-
-// const btn = document.querySelector('.button_color');
-// btn.addEventListener('click', e => {
-//   const id = parseInt(input.value);
-//   const theme = colorTheme.find(theme => theme.id === id);
-//   image.innerHTML = generatePictureTag(
-//     heroBackground,
-//     theme.path,
-//     theme.path_retina,
-//     'background_image'
-//   );
-//   setLocalData(id);
-// });
