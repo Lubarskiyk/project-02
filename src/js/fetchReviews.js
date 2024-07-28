3734b4b3f23eda3f9fc09e076c3f16e74961e33c
SergiiBartytskyi <108729124+SergiiBartytskyi@users.noreply.github.com>
2024-07-28 13:30:11 +0200

    bugfix/initSwiper
    - виправив помилку initSwiper

--- src/js/fetchReviews.js
+++ src/js/fetchReviews.js
@@ -5,54 +5,55 @@ import { onError } from './notifications';
 import Swiper from 'swiper';
 import { Keyboard, Navigation, Autoplay } from 'swiper/modules';
 
-const swiperReviews = new Swiper('.reviews-swiper', {
-  modules: [Navigation, Keyboard, Autoplay],
-  navigation: {
-    nextEl: '.reviews-swiper-button-right',
-    prevEl: '.reviews-swiper-button-left',
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
+function initSwiper() {
+  const swiperReviews = new Swiper('.reviews-swiper', {
+    modules: [Navigation, Keyboard, Autoplay],
+    navigation: {
+      nextEl: '.reviews-swiper-button-right',
+      prevEl: '.reviews-swiper-button-left',
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 16,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
     },
-    1440: {
-      slidesPerView: 4,
-      spaceBetween: 16,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
     },
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-  },
-});
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+  });
 
-swiperReviews.on('slideChange', () => {
-  const isEnd = swiperReviews.isEnd;
-  const isBeginning = swiperReviews.isBeginning;
+  swiperReviews.on('slideChange', () => {
+    const isEnd = swiperReviews.isEnd;
+    const isBeginning = swiperReviews.isBeginning;
 
-  if (isEnd) {
-    swiperReviews.autoplay.stop();
-    swiperReviews.params.autoplay.reverseDirection = true;
-    swiperReviews.autoplay.start();
-  } else if (isBeginning) {
-    swiperReviews.autoplay.stop();
-    swiperReviews.params.autoplay.reverseDirection = false;
-    swiperReviews.autoplay.start();
-  }
-});
-
+    if (isEnd) {
+      swiperReviews.autoplay.stop();
+      swiperReviews.params.autoplay.reverseDirection = true;
+      swiperReviews.autoplay.start();
+    } else if (isBeginning) {
+      swiperReviews.autoplay.stop();
+      swiperReviews.params.autoplay.reverseDirection = false;
+      swiperReviews.autoplay.start();
+    }
+  });
+}
 const swiperSection = document.querySelector('.reviews');
 const observer = new IntersectionObserver(
   entries => {
