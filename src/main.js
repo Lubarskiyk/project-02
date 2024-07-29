c32731198e6c74f9a7571ad1cd7af48bc797b631
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-07-29 14:12:53 +0300

    +

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { renderPhotoAboutme } from './js/about_up.js';
 import { renderHero } from './js/hero_background.js';
 import { hideTitle } from './js/covers.js';
 import '/js/covers.js';
+import '/js/about_me.js';
 
 renderHero();
 renderPhotoAboutme();
