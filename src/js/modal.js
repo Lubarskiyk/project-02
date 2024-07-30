5dcbe4f375b686d95691820e323ca4f2dd5a4617
DorianGray0 <d.boyko0509@gmail.com>
2024-07-30 17:35:15 +0300

    make modal css and html

---
+++ src/js/modal.js
@@ -0,0 +1,6 @@
+// import { handlerPost } from './handlerPost';
+// import { refs } from './refs.footer';
+
+// refs.form.addEventListener('submit', handlerPost);
+
+// refs.spinner.classList.add('is-hidden');
