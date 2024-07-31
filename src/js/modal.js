bf621c8e4c78533658fb466639cf20a4281d7e28
DorianGray0 <d.boyko0509@gmail.com>
2024-07-31 17:00:52 +0300

    add backdrop click

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,9 +9,12 @@ refs.spinnerModal.classList.add('is-hidden');
 refs.modalOrder.classList.add('is-hidden-modal');
 
 export function toggleModalClick(evt) {
-  // console.log(evt.target.tagName);
+  // console.log(evt.target);
   // const parentModal = evt.target.closest('.js-backdrop-order');
-  // console.log(parentModal);
+  // console.dir(parentModal);
+  // if (evt.target.dataset.modal) {
+  //   toggleModal();
+  // }
   const parentBtnClose = evt.target.closest('.js-modal-close-btn');
   const parentBtnSend = evt.target.closest('.js-spinner-wrap');
   if (parentBtnSend) {
