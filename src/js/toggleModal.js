ad5a94fb0a9451e43dd80a022a089acc5bb23098
DorianGray0 <d.boyko0509@gmail.com>
2024-07-31 20:13:30 +0300

    add Escape and bg in modal

--- src/js/toggleModal.js
+++ src/js/toggleModal.js
@@ -1,26 +1,26 @@
 import { refs } from './refs';
 
-document.addEventListener('keydown', handlerKey);
-
-function handlerKey(evt) {
-  if (evt.code === 'Escape') {
-    toggleModal();
-  }
-}
-
 function toggleModalClick(evt) {
   const backdropModal = evt.target.classList.contains('js-backdrop-order');
   const parentBtnClose = evt.target.closest('.js-modal-close-btn');
 
   if (backdropModal || parentBtnClose) {
     toggleModal();
+    handlerKey();
     document.removeEventListener('keydown', handlerKey);
   }
 }
 
 function toggleModal() {
+  document.addEventListener('keydown', handlerKey);
   refs.modalOrder.classList.toggle('is-hidden-modal');
   refs.body.classList.toggle('is-lock');
+}
+
+function handlerKey(evt) {
+  if (evt.code === 'Escape') {
+    toggleModal();
+  }
 }
 
 export { toggleModalClick, toggleModal };
